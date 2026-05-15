7e0a7bcf96a507d220cc9b21449e444647726129
Rick <rig12332113@gmail.com>
2026-05-14 23:44:24 -0700

    finish basic visual odometry, but it is pretty bad

--- compare_odom_paths.py
+++ compare_odom_paths.py
@@ -2,6 +2,7 @@
 
 import rclpy
 from rclpy.node import Node
+
 from nav_msgs.msg import Odometry, Path
 from geometry_msgs.msg import PoseStamped
 
@@ -10,57 +11,118 @@ class OdomPathCompare(Node):
     def __init__(self):
         super().__init__("odom_path_compare")
 
-        self.rtab_path = Path()
+        self.compare_frame = "compare"
+
         self.gt_path = Path()
+        self.rtab_path = Path()
+        self.vo_path = Path()
 
-        self.rtab_path.header.frame_id = "compare"
-        self.gt_path.header.frame_id = "compare"
+        self.gt_path.header.frame_id = self.compare_frame
+        self.rtab_path.header.frame_id = self.compare_frame
+        self.vo_path.header.frame_id = self.compare_frame
 
-        self.rtab_origin = None
         self.gt_origin = None
+        self.rtab_origin = None
+        self.vo_origin = None
 
-        self.rtab_pub = self.create_publisher(Path, "/compare/rtabmap_path", 10)
-        self.gt_pub = self.create_publisher(Path, "/compare/ground_truth_path", 10)
+        self.gt_pub = self.create_publisher(
+            Path,
+            "/compare/ground_truth_path",
+            10
+        )
 
-        self.create_subscription(Odometry, "/rtabmap/odom", self.rtab_callback, 10)
-        self.create_subscription(Odometry, "/ground_truth/odom", self.gt_callback, 10)
+        self.rtab_pub = self.create_publisher(
+            Path,
+            "/compare/rtabmap_path",
+            10
+        )
 
-        self.get_logger().info("Comparing /rtabmap/odom and /ground_truth/odom")
+        self.vo_pub = self.create_publisher(
+            Path,
+            "/compare/rgbd_vo_path",
+            10
+        )
+
+        self.create_subscription(
+            Odometry,
+            "/ground_truth/odom",
+            self.gt_callback,
+            10
+        )
+
+        self.create_subscription(
+            Odometry,
+            "/rtabmap/odom",
+            self.rtab_callback,
+            10
+        )
+
+        self.create_subscription(
+            Odometry,
+            "/rgbd_vo/odom",
+            self.vo_callback,
+            10
+        )
+
+        self.get_logger().info(
+            "Comparing /ground_truth/odom, /rtabmap/odom, and /rgbd_vo/odom"
+        )
 
     def make_pose(self, msg, origin):
         x = msg.pose.pose.position.x
         y = msg.pose.pose.position.y
+        z = msg.pose.pose.position.z
 
         if origin is None:
-            origin = (x, y)
+            origin = (x, y, z)
 
         pose = PoseStamped()
         pose.header.stamp = msg.header.stamp
-        pose.header.frame_id = "compare"
+        pose.header.frame_id = self.compare_frame
+
         pose.pose.position.x = x - origin[0]
         pose.pose.position.y = y - origin[1]
-        pose.pose.position.z = 0.0
+        pose.pose.position.z = z - origin[2]
+
         pose.pose.orientation = msg.pose.pose.orientation
 
         return pose, origin
 
-    def rtab_callback(self, msg):
-        pose, self.rtab_origin = self.make_pose(msg, self.rtab_origin)
-        self.rtab_path.header.stamp = msg.header.stamp
-        self.rtab_path.poses.append(pose)
-        self.rtab_pub.publish(self.rtab_path)
-
     def gt_callback(self, msg):
         pose, self.gt_origin = self.make_pose(msg, self.gt_origin)
+
         self.gt_path.header.stamp = msg.header.stamp
         self.gt_path.poses.append(pose)
+
         self.gt_pub.publish(self.gt_path)
 
+    def rtab_callback(self, msg):
+        pose, self.rtab_origin = self.make_pose(msg, self.rtab_origin)
 
-def main():
-    rclpy.init()
+        self.rtab_path.header.stamp = msg.header.stamp
+        self.rtab_path.poses.append(pose)
+
+        self.rtab_pub.publish(self.rtab_path)
+
+    def vo_callback(self, msg):
+        pose, self.vo_origin = self.make_pose(msg, self.vo_origin)
+
+        self.vo_path.header.stamp = msg.header.stamp
+        self.vo_path.poses.append(pose)
+
+        self.vo_pub.publish(self.vo_path)
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
     node = OdomPathCompare()
-    rclpy.spin(node)
+
+    try:
+        rclpy.spin(node)
+    except KeyboardInterrupt:
+        pass
+
     node.destroy_node()
     rclpy.shutdown()
 
--- src/rgbd_vo/rgbd_vo/rgbd_node.py
+++ src/rgbd_vo/rgbd_vo/rgbd_node.py
@@ -5,6 +5,8 @@ from cv_bridge import CvBridge
 from message_filters import Subscriber, ApproximateTimeSynchronizer
 import cv2
 import numpy as np
+from nav_msgs.msg import Path, Odometry
+from geometry_msgs.msg import PoseStamped
 
 class RGBD_Node(Node):
     def __init__(self):
@@ -31,8 +33,26 @@ class RGBD_Node(Node):
         self.ts.registerCallback(self.rgbdCallBack)
 
         self.camera_info_sub = self.create_subscription(
-            CameraInfo, "depth_camera/camera_info", self.cameraCallBack, 10
+            CameraInfo, "/depth_camera/camera_info", self.cameraCallBack, 10
         )
+
+        self.path_pub = self.create_publisher(
+            Path, "/rgbd_vo/path", 10
+        )
+        self.odom_pub = self.create_publisher(
+            Odometry, "/rgbd_vo/odom", 10
+        )
+        self.path_msg = Path()
+        self.path_msg.header.frame_id = "map"
+
+        self.has_camera_info = False
+        self.fx = None
+        self.fy = None
+        self.cx = None
+        self.cy = None
+        self.intrinsic = None
+
+        self.num_match = 100 # change this to param later
         self.bridge = CvBridge()
         self.orb = cv2.ORB_create(nfeatures=500)
         self.matcher = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
@@ -41,7 +61,14 @@ class RGBD_Node(Node):
         self.prev_depth = None
         self.prev_rgb = None
 
+        self.T_world_camera = np.eye(4)
+
+
     def rgbdCallBack(self, rgb_msg, depth_msg):
+        if not self.has_camera_info:
+            print("Waiting for camera_info...")
+            return
+
         rgb_image = self.bridge.imgmsg_to_cv2(rgb_msg, desired_encoding="rgb8")
         depth_image = self.bridge.imgmsg_to_cv2(depth_msg, desired_encoding="32FC1")
         gray = cv2.cvtColor(rgb_image, cv2.COLOR_RGB2GRAY)
@@ -72,29 +99,16 @@ class RGBD_Node(Node):
         else:
             valid_descriptors = None
 
-        print(f"Original keypoints: {len(keypoints)}")
-        print(f"Valid-depth keypoints: {len(valid_keypoints)}")
-
-        # debug_img = cv2.drawKeypoints(
-        #     rgb_image,
-        #     valid_keypoints,
-        #     None,
-        #     flags=cv2.DRAW_MATCHES_FLAGS_DEFAULT
-        # )
-
-        # cv2.imshow("ORB keypoints", debug_img)
-        # cv2.waitKey(1)
-
         if self.prev_descriptors is not None and valid_descriptors is not None:
             matches = self.matcher.match(self.prev_descriptors, valid_descriptors)
 
             # smaller distance means better descriptor match
             matches = sorted(matches, key=lambda m: m.distance)
 
-            good_matches = matches[:50]
+            good_matches = matches[:self.num_match]
 
-            print(f"Raw matches: {len(matches)}")
-            print(f"Good matches: {len(good_matches)}")
+            # print(f"Raw matches: {len(matches)}")
+            # print(f"Good matches: {len(good_matches)}")
 
             match_img = cv2.drawMatches(
                 self.prev_rgb,
@@ -106,28 +120,109 @@ class RGBD_Node(Node):
                 flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS
             )
 
-            cv2.imshow("ORB matches", match_img)
-            cv2.waitKey(1)
+            # cv2.imshow("ORB matches", match_img)
+            # cv2.waitKey(1)
         
+
+            # use previous 3D points and current 2D points to save PnP question
+            object_points = []
+            image_points = []
+            for match in good_matches:
+                prev_kp = self.prev_keypoints[match.queryIdx]
+                u_prev, v_prev = prev_kp.pt
+                u_prev = int(u_prev)
+                v_prev = int(v_prev)
+
+                curr_kp = valid_keypoints[match.trainIdx]
+                u_curr, v_curr = curr_kp.pt
+
+                Z = self.prev_depth[v_prev, u_prev]
+                X = (u_prev - self.cx) * Z / self.fx
+                Y = (v_prev - self.cy) * Z / self.fy
+                object_points.append([X, Y, Z])
+
+                image_points.append([u_curr, v_curr])
+
+            object_points = np.array(object_points, dtype=np.float32)
+            image_points = np.array(image_points, dtype=np.float32)
+
+            # run PnP solver 
+            success, rvec, t, inlier = cv2.solvePnPRansac(
+                object_points, image_points, self.intrinsic, np.zeros((5,1)),
+            ) 
+            R, _ = cv2.Rodrigues(rvec)
+
+            T_prev_curr = np.eye(4)
+            T_prev_curr[:3, :3] = R
+            T_prev_curr[:3, 3] = t.ravel()
+
+            T_curr_prev = np.linalg.inv(T_prev_curr)
+
+            self.T_world_camera = self.T_world_camera @ T_curr_prev
+
+            position = self.T_world_camera[:3, 3]
+
+            # Initialize the message and publish
+            pose_msg = PoseStamped()
+
+            pose_msg.header.stamp = self.get_clock().now().to_msg()
+            pose_msg.header.frame_id = "map"
+
+            pose_msg.pose.position.x = float(position[0])
+            pose_msg.pose.position.y = float(position[1])
+            pose_msg.pose.position.z = float(position[2])
+
+            pose_msg.pose.orientation.x = 0.0
+            pose_msg.pose.orientation.y = 0.0
+            pose_msg.pose.orientation.z = 0.0
+            pose_msg.pose.orientation.w = 1.0
+
+            self.path_msg.header.stamp = pose_msg.header.stamp
+            self.path_msg.poses.append(pose_msg)
+
+            self.path_pub.publish(self.path_msg)
+
+            odom_msg = Odometry()
+
+            odom_msg.header.stamp = self.get_clock().now().to_msg()
+            odom_msg.header.frame_id = "map"
+            odom_msg.child_frame_id = "camera_link_optical"
+
+            odom_msg.pose.pose.position.x = float(position[0])
+            odom_msg.pose.pose.position.y = float(position[1])
+            odom_msg.pose.pose.position.z = float(position[2])
+
+            odom_msg.pose.pose.orientation.x = 0.0
+            odom_msg.pose.pose.orientation.y = 0.0
+            odom_msg.pose.pose.orientation.z = 0.0
+            odom_msg.pose.pose.orientation.w = 1.0
+
+            self.odom_pub.publish(odom_msg)
+
+        # update frame
         self.prev_keypoints = valid_keypoints
         self.prev_descriptors = valid_descriptors
         self.prev_depth = depth_image
         self.prev_rgb = rgb_image
 
     def cameraCallBack(self, msg):
-        # print("camera_info")
-        intrinsic = msg.k
-        # print(f"fx = {intrinsic[0]},  \
-        #         fy = {intrinsic[4]},  \
-        #         cx = {intrinsic[2]},  \
-        #         cy = {intrinsic[5]}")
+        if (not self.has_camera_info):
+            self.fx = msg.k[0]
+            self.fy = msg.k[4]
+            self.cx = msg.k[2]
+            self.cy = msg.k[5]
+            self.intrinsic = msg.k.reshape((3,3))
+            self.has_camera_info = True
+            print("get camera info")
+        else:
+            return
 
 def main(args=None):
     rclpy.init(args=args)
     node = RGBD_Node()
     rclpy.spin(node)
     node.destroy_node()
-    rclpy.shutdown
+    rclpy.shutdown()
 
 if __name__ == "__main__":
     main()
